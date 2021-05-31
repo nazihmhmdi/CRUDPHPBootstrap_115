38ec731381cc6eeabb2fd1306e5a79d225199035
Muhammad Nazih Mahmudi <nazih.mahmudi12@gmali.com>
2021-05-31 19:36:31 +0800

    modal

--- index.php
+++ index.php
@@ -1,12 +1,15 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en">  
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.datatables.net/1.10.24/css/dataTables.bootstrap5.min.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css">
   <title>Data Mahasiswa</title>
 </head>
+
 <body>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
@@ -19,13 +22,13 @@
           <a class="nav-link active" aria-current="page" href="create.php">Tambah Mahasiswa</a>
           <a class="nav-link" href="#">Features</a>
           <a class="nav-link" href="#">Pricing</a>
-        </div>
+        </div> 
       </div>
-    </div>
+    </div> 
   </nav>
 
   <div class="container data-mahasiswa mt-5">
-    <table class="table table-striped">
+    <table class="table table-striped" id="tabelMahasiswa">
       <thead>
         <tr>
           <th scope="col">No. </th>
@@ -36,6 +39,7 @@
         </tr>
       </thead>
       <tbody>
+
         <?php
         //Memanggil config.php yang sudah kita buat
         include 'config.php';
@@ -68,9 +72,60 @@
         <?php
         }
         ?>
+
+        <div class="container data-mahasiswa mt-5">
+         <!-- Button trigger modal -->
+        <button type="button" class="btn btn-primary mb-3" data-bs-toggle="modal" data-bs-target="#tambahData">
+          Tambah Data
+        </button>
+        <!-- Modal -->
+        <div class="modal fade" id="tambahData" tabindex="-1" aria-labelledby="tambahDataLabel" aria-hidden="true">
+          <div class="modal-dialog">
+            <div class="modal-content">
+            <!-- Membuat form dengan method post untuk memanggil file store.php -->
+              <form method="post" action="create.php" name="form">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="tambahDataLabel">Modal title</h5>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                <!-- Input nama -->
+                  <div class="mb-3">
+                    <label for="nama" class="form-label">Nama</label>
+                    <input type="text" class="form-control" id="nama" placeholder="Masukkan Nama Mahasiswa" name="nama" required>
+                  </div>
+                <!-- Input nim -->
+                  <div class="mb-3">
+                    <label for="NIM" class="form-label">NIM</label>
+                    <input type="text" class="form-control" id="NIM" placeholder="Masukkan NIM Mahasiswa" name="nim" required>
+                  </div>
+                <!-- Input Alamat -->
+                  <div class="mb-3">
+                    <label for="Alamat" class="form-label">Alamat</label>
+                    <textarea type= "text" class="form-control" id="Alamat" placeholder="Masukkan Alamat Mahasiswa" name="alamat" required></textarea>
+                  </div>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                  <button type="submit" class="btn btn-primary" value="SIMPAN">Tambah</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+
         </tbody>
     </table>
   </div>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+  <script src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
+  <script src="https://cdn.datatables.net/1.10.24/js/dataTables.bootstrap5.min.js"></script>
+  <script>
+    $(document).ready(function() {
+      $('#tabelMahasiswa').DataTable();
+    });
+  </script>
 </body>
+
 </html>
